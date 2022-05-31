be241f53164ebb997f5fdf472db129c1bd050ed4
sicatj <js8464h@gmail.com>
2022-05-31 12:19:38 +0800

    admin products changes

--- scripts/productController.js
+++ scripts/productController.js
@@ -79,6 +79,27 @@ class ProductController {
         
         }
 
+        displayAdminProductsTable() {
+            let insertProduct = "";
+            this.allProductItems.forEach (productItem => {     
+                insertProduct +=
+                        // <!-- product item row-->
+                `
+                <tr>
+                <td>${productItem.name}</td>
+                <td>${productItem.category}</td>
+                <td>${productItem.details}</td>
+                <td>${productItem.price}</td>
+                <td>${productItem.imageURL}</td>
+                <td><button id="editBtn"><a onclick="onEdit(this)" >Edit</a></button>
+                <button id="deleteBtn"<a onclick="onDelete(this)">Delete</a></button></td>
+                </tr>
+                `
+    
+            });
+            document.getElementById("productList").getElementsByTagName('tbody')[0].innerHTML =  insertProduct;
+        }
+
     //filterProduct Method
     
         filterProduct(getSelectedCategory) 
